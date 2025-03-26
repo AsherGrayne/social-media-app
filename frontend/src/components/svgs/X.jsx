b0e8a6707cf125aabcb741f88245274c0836b102
Pratick Kumar Choudhury <128919790+AsherGrayne@users.noreply.github.com>
2025-03-26 22:02:34 +0530

    Initial commit

---
+++ frontend/src/components/svgs/X.jsx
@@ -0,0 +1,6 @@
+const XSvg = (props) => (
+	<svg aria-hidden='true' viewBox='0 0 24 24' {...props}>
+		<img src="C:\Users\profe\Desktop\twitter-clone\frontend\src\components\svgs\icon.jpeg"></img>	
+	</svg>
+);
+export default XSvg;
